0b2c13201054070faa5591ce1a0d8329fb79127c
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2023-04-13 09:50:57 +0700

    refactoring pada Tax Function

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -3,49 +3,38 @@ package lib;
 public class TaxFunction {
 
 	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus
-	 * dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan
-	 * bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan)
-	 * dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena
-	 * pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah
-	 * sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya
-	 * ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
+	 * Calculates the amount of income tax that an employee has to pay annually.
+	 *
+	 * Tax is calculated as 5% of the annual net income (monthly salary and other
+	 * monthly income multiplied by the number of months worked minus the
+	 * deductible) minus the tax-free income.
+	 *
+	 * If the employee is unmarried and has no children, then the tax-free income is
+	 * Rp 54,000,000.
+	 * If the employee is married, the tax-free income is increased by Rp 4,500,000.
+	 * If the employee has children, the tax-free income is increased by Rp
+	 * 4,500,000 per child up to the third child.
+	 *
 	 */
 
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
+	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthsWorked, int deductible,
 			boolean isMarried, int numberOfChildren) {
 
-		int tax = 0;
-
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
+		if (numberOfMonthsWorked > 12) {
+			throw new IllegalArgumentException("Number of months worked cannot exceed 12.");
 		}
 
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
+		numberOfChildren = Math.min(numberOfChildren, 3);
 
+		int taxFreeIncome = 54000000;
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible
-					- (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		} else {
-			tax = (int) Math.round(
-					0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			taxFreeIncome += 4500000;
 		}
+		taxFreeIncome += numberOfChildren * 4500000;
 
-		if (tax < 0) {
-			return 0;
-		} else {
-			return tax;
-		}
+		int taxableIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthsWorked - deductible - taxFreeIncome;
+		int tax = (int) Math.round(0.05 * taxableIncome);
 
+		return Math.max(tax, 0);
 	}
-
 }
